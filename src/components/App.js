459179e2ac4acf8cd316898b0524f7b4f4db946c
EMMANUEL <waruterewachira7@gmail.com>
2024-06-03 00:56:16 +0300

    update

--- src/components/App.js
+++ src/components/App.js
@@ -11,10 +11,19 @@ function App() {
       <div className="App">
         <h1>BMI Calculator</h1>
         <BMIForm />
-        <BMIDisplay />
+        <BMIDisplay /> 
       </div>
     </BMIProvider>
   );
 }
 
 export default App;
+
+
+
+
+
+
+//BMIProvider: Wraps all child components, making the context values and functions available to BMIForm and BMIDisplay.
+//BMIForm: Renders the form component for user input.Allows users to input their weight and height.
+//BMIDisplay: Renders the component that displays the calculated BMI.Shows the calculated BMI based on the user input
